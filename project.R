c8c9e2481823b4c429509bf816dd1d187df15ad6
Arther-38 <55568214+Arther-38@users.noreply.github.com>
2020-05-24 00:39:16 +0800

    now

--- project.R
+++ project.R
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-rm(list=ls())
-df<-read.csv('data/example_data.csv')
-print("hi")
-print(hi)
-=======
+
 rm(list=ls())
 df<-read.csv('data/example_data.csv')
 print(hi)
->>>>>>> 656715b05df6933418e67f3e1566c8dd4e87196c
+
